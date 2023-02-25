508278b6dd9b394613b374bd1a19d5ba66713d0c
Thierry CHANG-FONG <81683083+TCFproject@users.noreply.github.com>
2023-02-25 10:26:41 +0100

    mise à jour test et modif .bat
    Le .bat reboot execute les tests égalements

--- rebootBDD.bat
+++ rebootBDD.bat
@@ -2,3 +2,5 @@ php bin/console doctrine:database:drop --force
 php bin/console doctrine:database:create
 echo yes | symfony console doctrine:migrations:migrate --no-interaction
 sqlite3 var/data.db "INSERT INTO role(label) VALUES('Botaniste'),('Proprietaire')"
+php bin/phpunit ./tests/PropriServiTest.php
+php bin/phpunit ./tests/BotaServiTest.php
