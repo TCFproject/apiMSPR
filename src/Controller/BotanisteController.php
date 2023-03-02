18685b23706dfafbf8b24afcdf497559d82829fd
Thierry CHANG-FONG <81683083+TCFproject@users.noreply.github.com>
2023-03-02 17:12:59 +0100

    a

--- src/Controller/BotanisteController.php
+++ src/Controller/BotanisteController.php
@@ -69,9 +69,9 @@ class BotanisteController extends AbstractController
             return $object->getId();
         }]);
 
-        $response = new Response($identity);
+        $response = new Response($identity);/*
         $response->headers->set('Access-Control-Allow-Origin', '*');
-        $response->headers->set('Content-Type', 'application/json;charset=UTF-8');
+        $response->headers->set('Content-Type', 'application/json;charset=UTF-8');*/
         return $response;
     }
 
